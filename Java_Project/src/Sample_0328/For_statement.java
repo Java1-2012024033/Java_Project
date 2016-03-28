aa58449025d125015ac1b5f9f0ea3424eca128eb
basic <basic@E40>
2016-03-28 20:50:22 +0900

    PpiGon

---
+++ Java_Project/src/Sample_0328/For_statement.java
@@ -0,0 +1,18 @@
+package Sample_0328;
+
+public class For_statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10; i++){
+			System.out.println(i);
+		}
+		int j=10;
+		while(j>=1){//for(int j =10; j>=1; j--)
+			System.out.println(j*10);//j*10
+			j--;
+		}
+		
+	}
+
+}
---
+++ Java_Project/src/Sample_0328/GuGuDan.java
@@ -0,0 +1,15 @@
+package Sample_0328;
+
+public class GuGuDan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1; i<=9; i++){
+			for(int j=3; j<=9; j+=2){
+				System.out.print(j+"*"+i+"="+(i*j)+" ");
+				if(j==9) System.out.println(" ");
+			}
+		}
+	}
+
+}
