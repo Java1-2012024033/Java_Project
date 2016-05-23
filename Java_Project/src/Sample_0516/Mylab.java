701a400c76744a7214a97ca90fa5a755eeddb820
YEON <YEON@YEON-PC>
2016-05-23 12:47:41 +0900

    Calculator

--- Java_Project/src/Sample_0516/Mylab.java
+++ Java_Project/src/Sample_0516/Mylab.java
@@ -1,5 +1,8 @@
 package Sample_0516;
 
+import java.awt.LayoutManager;
+
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
---
+++ Java_Project/src/Sample_0523/CalculatorTest.java
@@ -0,0 +1,11 @@
+package Sample_0523;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator C = new Calculator();
+
+	}
+
+}
